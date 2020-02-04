fb29cb76ddbee358a79f153d4e2b05a06457dfbf
Brayan Lopez <brayan9105@hotmail.com>
2020-02-04 11:11:20 -0500

    Using constructor function

---
+++ index.js
@@ -0,0 +1,117 @@
+// Requirement 1
+
+function Square(side){
+    this.side = side
+}
+
+Square.prototype.perimeter= function(){return this.side*4}
+Square.prototype.area= function(){return Math.pow(this.side, 2)}
+
+const s1 = new Square(5)
+console.log(s1.perimeter())
+console.log(s1.area())
+
+//Using class keyword
+/*
+class Square{
+    constructor(side){
+        this.side=side
+    }
+
+    perimeter(){return this.side*4}
+    area(){return Math.pow(this.side,2)}
+}
+
+const s1 = new Square(5)
+console.log(s1.perimeter())
+console.log(s1.area())
+*/
+
+
+
+// Requirement 2
+
+function Person(name, weight, height){
+    this.name = name
+    this.weight = weight
+    this.height = height
+}
+
+Person.prototype.bmi = function(){return Math.pow((this.weight/this.height), 2)}
+Person.prototype.greet = function(person){ return `Hi ${person.name} muy name is ${this.name}`}
+
+const p1 = new Person('Pedro', 72, 1.5)
+const p2 = new Person('Maria', 54, 1.7)
+
+console.log(p1.greet(p2))
+console.log(p2.greet(p1))
+
+// Using class keyword
+/*
+class Person{
+    constructor(name, weight, height){
+        this.name = name
+        this.weight = weight
+        this.height = height
+    }
+
+    bmi(){return Math.pow((this.weight/this.height), 2)}
+    greet(person){ return `Hi ${person.name} muy name is ${this.name}`}
+
+}
+
+const p1 = new Person('Pedro', 72, 1.5)
+const p2 = new Person('Maria', 54, 1.7)
+
+console.log(p1.greet(p2))
+console.log(p2.greet(p1))
+*/
+
+// requiment 3
+
+function Room(number){
+    this.number = number
+    this.available = true
+    this.guests = 0
+}
+
+const r1 = new Room(101)
+
+console.log(`${r1.number}\n${r1.available}\n${r1.guests}`)
+
+
+// LeetCode 771. Jewels and Stones
+
+const numJewelsInStones = (j, s) => {
+    let stonesArr = s.split('')
+    let jewelsArr = j.split('')
+    let countJewels = 0
+    for(let i=0;i<stonesArr.length;i++){
+        for(let x=0;x<jewelsArr.length;x++){
+            if(stonesArr[i] === jewelsArr[x]){
+                countJewels++;
+                break;
+            }
+        }
+    }
+
+    return countJewels;
+}
+
+console.log(numJewelsInStones("aA", "aAAbbbb"))
+console.log(numJewelsInStones("z", "ZZ"))
+
+// ES6 solution
+/*
+const numJewelsInStones = (j, s) => {
+    let stonesArr = s.split('')
+    let jewelsArr = j.split('')
+    let countJewels=0
+    stonesArr.forEach((stone) => jewelsArr.forEach(j => {
+        if(stone === j){countJewels++}
+    }))
+    return countJewels
+}
+console.log(numJewelsInStones("aA", "aAAbbbb"))
+console.log(numJewelsInStones("z", "ZZ"))
+*/
